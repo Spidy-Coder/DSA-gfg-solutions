f6365eaf4192afa7f28c143544fd9a9df23661e4
Paridhi Jain <18ec.paridhijain@gmail.com>
2021-07-05 16:28:42 +0530

    Add files via upload

---
+++ Arrays/reverse_an_array.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int Reverse(int arr[],int n){
+    int low=0,high=n-1;
+    while(low<high){
+        int temp = arr[low];
+        arr[low] = arr[high];
+        arr[high]=temp;
+        low++;
+        high--;
+    }
+    
+}
+
+//driver code
+
+int main() {
+	
+      int arr[] = {10, 5, 7, 30}, n = 4;
+
+      cout<<"Before Reverse"<<endl;
+
+       for(int i = 0; i < n; i++)
+       {
+       		cout<<arr[i]<<" ";
+       }
+
+       cout<<endl;
+
+       Reverse(arr, n);
+
+       cout<<"After Reverse"<<endl;
+
+       for(int i = 0; i < n; i++)
+       {
+       		cout<<arr[i]<<" ";
+       }
+    
+}
