875b78d3bc17eb2f969e024f018506f99ec48e1d
Paridhi Jain <18ec.paridhijain@gmail.com>
2021-08-12 16:02:19 +0530

    Update bubble sort.cpp

--- Sorting/bubble sort.cpp
+++ Sorting/bubble sort.cpp
@@ -1,1 +1,23 @@
+class Solution
+{
+    public:
+    
+    void swap(int *a,int *b){
+        int temp;
+        temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+    //Function to sort the array using bubble sort algorithm.
+    void bubbleSort(int arr[], int n)
+    {
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    swap(&arr[j],&arr[j+1]);
+                }
+            }
+        }
+    }
+};
 
