3c35e72e27c5ce3ac37eb525fee9a13ac4f93fee
Paridhi Jain <18ec.paridhijain@gmail.com>
2021-11-19 22:36:00 +0530

    Create Preorder traversal.cpp

---
+++ Binary Tree/Preorder traversal.cpp
@@ -0,0 +1,32 @@
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child  
+
+/*
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+//Function to return a list containing the preorder traversal of the tree.
+void PREORDER(Node* root,vector<int> &v){
+    if(root!=NULL){
+        v.push_back(root->data);
+        PREORDER(root->left,v);
+        PREORDER(root->right,v);
+    }
+}
+vector <int> preorder(Node* root)
+{
+  // Your code here
+  vector<int> v;
+  PREORDER(root,v);
+  return v;
+}
